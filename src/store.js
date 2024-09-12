07b06ed6ca8f0146a453be0d889625cb21d7ba55
Brandt Jorgensen <brandt.jorgensen@datasite.com>
2024-09-12 10:48:01 -0500

    search params to redux

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 // instatiate redux store
 import { configureStore } from "@reduxjs/toolkit";
 import adoptedPet from "./adoptedPetSlice";
+import searchParams from "./searchParamsSlice";
 
 const store = configureStore({
   reducer: {
     adoptedPet,
+    searchParams,
   },
 });
 
